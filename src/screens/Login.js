1e39cd0c305d3a80015f4f156489f59ed2972d41
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-08-11 14:43:24 +0700

    make more like design

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,21 +3,63 @@ import { SafeAreaView, View, StyleSheet } from 'react-native';
 // components
 import Text from '../components/Text';
 import SignInWith from '../components/SignInWith';
+import Line from '../components/Line';
 // utils
 import { normalize } from '../utils/normalize';
 import * as Theme from '../utils/theme';
+// icons
+import GoogleIcon from '../assets/icons/iconGoogle.svg';
+import FacebookIcon from '../assets/icons/iconFacebook.svg';
+import AppleIcon from '../assets/icons/iconApple.svg';
 
 const Login = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.title}>
-          <Text>Talky</Text>
-          <Text style={styles.dot}>.</Text>
+        <Text>
+          <Text size={60} weight={800} style={styles.title}>
+            Talky
+          </Text>
+          <Text size={60} weight={800} style={styles.dot}>
+            .
+          </Text>
         </Text>
       </View>
-      <View>
-        <SignInWith title="Google" />
+      <View style={styles.body}>
+        <SignInWith
+          Icon={GoogleIcon}
+          text="Sign in with Google"
+          rounded={8}
+          style={styles.button}
+        />
+        <SignInWith
+          Icon={FacebookIcon}
+          text="Sign in with Facebook"
+          rounded={8}
+          style={styles.button}
+        />
+        <SignInWith
+          Icon={AppleIcon}
+          text="Sign in with Apple"
+          rounded={8}
+          style={styles.button}
+        />
+        <View style={styles.horizontalRule}>
+          <Line />
+          <Text style={styles.horizontalRuleText}>or</Text>
+          <Line />
+        </View>
+        <SignInWith
+          text="Continue with phone number"
+          rounded={8}
+          style={styles.button}
+        />
+      </View>
+      <View style={styles.footer}>
+        <Text size={14}>Already have an account?</Text>
+        <Text size={14} color={Theme.primary}>
+          Sign up here
+        </Text>
       </View>
     </SafeAreaView>
   );
@@ -27,17 +69,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Theme.background,
+    paddingHorizontal: normalize(32),
   },
+  // header
   header: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: normalize(16),
   },
-  title: {
-    fontSize: normalize(60),
-  },
+  title: {},
   dot: {
     color: Theme.primary,
+  },
+  // body
+  body: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    marginBottom: normalize(16),
+  },
+  horizontalRule: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: normalize(16),
+  },
+  horizontalRuleText: {
+    marginHorizontal: normalize(24),
+  },
+  // footer
+  footer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
