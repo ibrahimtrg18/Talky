789ec9e152e23a0d3afa48a9f807ffef68b3a342
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-08-26 17:07:16 +0700

    add new theme colors

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,6 +1,7 @@
 export const text = '#243443';
 export const background = '#F7F7F9';
 export const line = '#58616A';
+export const border = '#AAB0B7';
 
 export const primary = '#377DFF';
 export const lightPrimary = '#E5F1FF';
@@ -8,3 +9,5 @@ export const lightPrimary = '#E5F1FF';
 export const secondary = '#2DCA8C';
 
 export const dark = '#58616A';
+export const white = '#FFFFFF';
+export const black = '#000000';
