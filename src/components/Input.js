f3f59a2b7e0b0f5f1f5c43bea9ce4ddaf856387e
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-08-26 17:07:30 +0700

    add custom input

---
+++ src/components/Input.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+// utils
+import { normalize } from '../utils/normalize';
+import * as Theme from '../utils/theme';
+import { getFontWeight } from '../utils/getFontWeight';
+// icons
+import EyeIcon from '../assets/icons/iconEye.svg';
+
+const Input = ({
+  size,
+  weight,
+  rounded,
+  style,
+  type,
+  showPassword,
+  setShowPassword,
+  ...props
+}) => {
+  const [isFocus, setIsFocus] = useState(false);
+
+  const customFontSize = size && { fontSize: normalize(size) };
+  const customRounded = rounded && { borderRadius: rounded };
+  const customWeight = getFontWeight(weight || 500);
+
+  const onTextInputFocus = () => {
+    setIsFocus({
+      backgroundColor: Theme.white,
+      borderColor: Theme.primary,
+      color: Theme.text,
+    });
+  };
+
+  const onTextInputBlur = () => {
+    setIsFocus({
+      backgroundColor: Theme.background,
+      borderColor: Theme.border,
+      color: Theme.text,
+    });
+  };
+
+  if (type === 'password') {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          {...props}
+          style={{
+            ...styles.input,
+            ...customFontSize,
+            ...customWeight,
+            ...customRounded,
+            ...isFocus,
+            ...style,
+          }}
+          onFocus={onTextInputFocus}
+          onBlur={onTextInputBlur}
+        />
+        <EyeIcon
+          style={styles.icon}
+          onPress={() => setShowPassword(!showPassword)}
+        />
+      </View>
+    );
+  }
+
+  return (
+    <TextInput
+      {...props}
+      style={{
+        ...styles.input,
+        ...customFontSize,
+        ...customWeight,
+        ...customRounded,
+        ...isFocus,
+        ...style,
+      }}
+      onFocus={onTextInputFocus}
+      onBlur={onTextInputBlur}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+  },
+  icon: {
+    position: 'absolute',
+    top: '50%',
+    right: 16,
+    transform: [{ translateY: -23 }],
+  },
+  input: {
+    backgroundColor: Theme.background,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: Theme.border,
+    color: Theme.text,
+    fontSize: normalize(14),
+    paddingVertical: normalize(12),
+    paddingHorizontal: normalize(10),
+  },
+});
+
+export default Input;
