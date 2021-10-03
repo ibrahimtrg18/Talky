81fb67e3e3c2a4c0b120d6e8db6d139b1ff9cdef
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-10-03 18:14:41 +0700

    put search user to redux store

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,13 +1,17 @@
-import { FETCH_PROFILE } from '../actions/user';
+import _ from 'lodash';
+import { FETCH_PROFILE, SEARCH_USER } from '../actions/user';
 
 const INITIAL_STATE = {
   profile: null,
+  search: {},
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FETCH_PROFILE:
       return { ...state, profile: action.payload };
+    case SEARCH_USER:
+      return { ...state, search: { ..._.mapKeys(action.payload, 'id') } };
     default:
       return state;
   }
