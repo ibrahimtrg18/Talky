5f0e172d880be55fda70338706bd516486cbcf0c
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-10-10 14:53:33 +0700

    style theme: add new danger color

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -8,6 +8,8 @@ export const lightPrimary = '#E5F1FF';
 
 export const secondary = '#2DCA8C';
 
+export const danger = '#bf1d1d';
+
 export const dark = '#58616A';
 export const white = '#FFFFFF';
 export const black = '#000000';
