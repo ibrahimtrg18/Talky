af4c4aeb2e5d7626376a03e4d9f70fb57cf08281
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-06 16:16:49 +0700

    restructure conversation initialstate add handle reducer for fetch conversation chat

--- src/redux/reducers/conversationReducer.js
+++ src/redux/reducers/conversationReducer.js
@@ -1,14 +1,19 @@
 import _ from 'lodash';
-import { FETCH_CONVERSATION } from '../actions/conversation';
+import {
+  FETCH_CONVERSATION,
+  FETCH_CONVERSATION_CHAT,
+} from '../actions/conversation';
 
 const INITIAL_STATE = {
-  conversation: null,
+  chat: {},
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FETCH_CONVERSATION:
-      return { ...state, conversation: action.payload };
+      return { ...state, ...action.payload };
+    case FETCH_CONVERSATION_CHAT:
+      return { ...state, chat: { ..._.mapKeys(action.payload.chats, 'id') } };
     default:
       return state;
   }
