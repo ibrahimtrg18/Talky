bf1aa008f387c3b34cd19c458f07cbb36fad47a6
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-06 16:15:52 +0700

    add action fetchConversationChatById for fetch chat conversation by conversation id fix not export type action

--- src/redux/actions/conversation.js
+++ src/redux/actions/conversation.js
@@ -1,6 +1,7 @@
 import axios from '../../utils/axios';
 
-const FETCH_CONVERSATION = 'FETCH_CONVERSATION';
+export const FETCH_CONVERSATION = 'FETCH_CONVERSATION';
+export const FETCH_CONVERSATION_CHAT = 'FETCH_CONVERSATION_CHAT';
 
 export const fetchConversationById = (id) => async (dispatch, getState) => {
   try {
@@ -8,7 +9,22 @@ export const fetchConversationById = (id) => async (dispatch, getState) => {
     const conversation = await axios.get(`/conversation/${id}`, {
       headers: { Authorization: 'Bearer ' + auth.access_token },
     });
-    dispatch({ type: FETCH_CONVERSATION, payload: conversation });
+    dispatch({ type: FETCH_CONVERSATION, payload: conversation.data });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const fetchConversationChatById = (id) => async (dispatch, getState) => {
+  try {
+    const { auth } = getState();
+    const conversation = await axios.get(`/conversation/${id}/chat`, {
+      headers: { Authorization: 'Bearer ' + auth.access_token },
+    });
+    dispatch({
+      type: FETCH_CONVERSATION_CHAT,
+      payload: conversation.data,
+    });
   } catch (e) {
     console.error(e);
   }
