47ed0fea86c82f6908253d8f6eabb29e6d951ff0
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-21 13:50:52 +0700

    refactoring: change upload image using react-native-image-crop-picker

--- src/screens/Setting/EditAvatar.js
+++ src/screens/Setting/EditAvatar.js
@@ -1,7 +1,8 @@
 // libraries
 import React from 'react';
 import { SafeAreaView, View, Image, StyleSheet } from 'react-native';
-import { launchImageLibrary } from 'react-native-image-picker';
+import Config from 'react-native-config';
+import ImagePicker from 'react-native-image-crop-picker';
 // components
 import Header from '../../components/Header';
 import Button from '../../components/Button';
@@ -13,10 +14,17 @@ const EditAvatar = () => {
   const [photo, setPhoto] = React.useState(null);
 
   const onSelectPhoto = () => {
-    launchImageLibrary({ noData: true }, (response) => {
-      console.log(response);
-      if (response) {
-        setPhoto(response);
+    ImagePicker.openPicker({
+      width: 300,
+      height: 300,
+      compressImageMaxWidth: 300,
+      compressImageMaxHeight: 300,
+      cropping: true,
+    }).then((image) => {
+      console.log(JSON.stringify(image, null, 2));
+
+      if (image) {
+        setPhoto(image);
       }
     });
   };
@@ -27,7 +35,7 @@ const EditAvatar = () => {
       <View style={styles.content}>
         {photo && (
           <>
-            <Image source={{ uri: photo.assets[0].uri }} style={styles.image} />
+            <Image source={{ uri: photo.path }} style={styles.image} />
           </>
         )}
         <Button title="Choose Photo" rounded={8} onPress={onSelectPhoto} />
